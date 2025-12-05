e1db754d624d96aff53bbbe0b72754725809adae
milot <rothree686@gmail.com>
2025-12-05 20:43:07 +0800

    add bomb

--- Assets/Scripts/Character/Player/ThrowableItem.cs
+++ Assets/Scripts/Character/Player/ThrowableItem.cs
@@ -9,12 +9,16 @@ namespace Character.Player
         [SerializeField] private float torqueForce = 100f;
         public Vector3 startPos;
         public Vector3 targetPos;
-        private Rigidbody2D _rb;
         private Vector2 _targetPos2D;
-        private bool _isMoving = true;
+        private Animator _bombAnimator;
+        private Rigidbody2D _rb;
+        private static readonly int IsExplode = Animator.StringToHash("IsExplode");
+        private readonly bool _isMoving = true;
         private void Start()
         {
+            _bombAnimator = GetComponent<Animator>();
             _rb = GetComponent<Rigidbody2D>();
+            _bombAnimator.SetBool(IsExplode,false);
             _targetPos2D = new Vector2(targetPos.x, targetPos.y);
             Vector2 direction = (_targetPos2D - (Vector2)transform.position).normalized;
             _rb.velocity = direction * moveSpeed;
@@ -27,8 +31,14 @@ namespace Character.Player
             float distance = Vector2.Distance(currentPos, _targetPos2D);
             if (distance < 0.2f)
             {
-                Destroy(gameObject);
+                _rb.velocity = Vector2.zero;
+                _rb.angularVelocity = 0;
+                _bombAnimator.SetBool(IsExplode,true);
             }
+        }
+        void DeleteBomb()
+        {
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Scripts/Splash/SplashPool.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Splash
+{
+    public class SplashPool : MonoBehaviour
+    {
+        void Start()
+        {
+        
+        }
+        void Update()
+        {
+        
+        }
+    }
+}
